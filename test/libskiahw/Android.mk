ae5b6e1d657e860e9a747e4f739426ba5eaab443
Saravanan Solaiyappan <saravanan.s@ti.com>
2011-11-23 18:21:58 -0600

    Skia: Test application
    This ports the SklibTiJpeg_Test test app from ICS:
    http://review.omapzoom.org/#/c/17978/
    
    Change-Id: Ie023ac927321facc4ad7607a883f2b67f9af547e
    Signed-off-by: Solaiyappan Saravanan <saravanan.s@ti.com>
    Signed-off-by: Daniel Levin <dendy@ti.com>

---
+++ test/libskiahw/Android.mk
@@ -0,0 +1,49 @@
+#
+# Copyright (C) 2012 Texas Instruments Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_PATH:= $(call my-dir)
+
+#ifdef HARDWARE_OMX
+################################################
+
+include $(CLEAR_VARS)
+
+LOCAL_SHARED_LIBRARIES := libskia
+
+LOCAL_WHOLE_STATIC_LIBRARIES := libc_common
+
+LOCAL_SRC_FILES := SkLibTiJpeg_Test.cpp
+
+LOCAL_MODULE := SkLibTiJpeg_Test
+LOCAL_MODULE_TAGS:= optional
+
+LOCAL_C_INCLUDES += \
+    external/skia/include/images \
+    external/skia/include/core \
+    bionic/libc/bionic
+
+
+ifeq ($(TARGET_BOARD_PLATFORM),omap4)
+    LOCAL_CFLAGS += -DTARGET_OMAP4
+endif
+
+
+include $(BUILD_EXECUTABLE)
+
+################################################
+#endif
+
+
