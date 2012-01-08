16aa5f8816dded26488f1bd1128b68013b1df574
Gustavo Diaz Prado <a0273371@ti.com>
2012-01-07 20:31:39 -0600

    HWC: Send Bltsville blit data in Post2
    This patch enables the HWC to send Bltsville blit data in the Post2
    call along with the DSS pipes configuration.
    
    A simple test policy is being used to blit. Blit everything if we can,
    otherwise fallback to the default DSS pipe policy already in place.
    
    Change-Id: Ic5e182a4cd5e9b8f896f695d71a89e8e1ad8e4e3
    Signed-off-by: Gustavo Diaz Prado <a0273371@ti.com>
    Signed-off-by: Tony Lofthouse <a0741364@ti.com>

--- hwc/Android.mk
+++ hwc/Android.mk
@@ -8,7 +8,7 @@ LOCAL_PRELINK_MODULE := false
 LOCAL_ARM_MODE := arm
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/../vendor/lib/hw
 LOCAL_SHARED_LIBRARIES := liblog libEGL libcutils libutils libhardware libhardware_legacy libz
-LOCAL_SRC_FILES := hwc.c
+LOCAL_SRC_FILES := hwc.c rgz_2d.c
 LOCAL_STATIC_LIBRARIES := libpng
 
 LOCAL_MODULE_TAGS := optional
