838f71dbea65c450e261c975b507e68fc50cb06c
Pavel Nedev <pnedev@mm-sol.com>
2012-02-21 16:49:55 +0200

    CameraHal: Implementation for CAF status callbacks
    Author: Tyler Luu <tluu@ti.com>
    
    1. Remove internal handling of CAMERA_MSG_FOCUS. Before we were
    enabling CAMERA_MSG_FOCUS only when application calls autoFocus().
    No longer needed since the CAMERA_MSG_FOCUS can come just during
    preview now.
    
    2. Add handling of unregistered focus callbacks. Since OMXCamera
    will send callbacks even when we are not waiting for one, add some
    handling.
    
    Change-Id: I70842a0b8f004873d728ff178ae3a4db12060a7c
    Signed-off-by: Tyler Luu <tluu@ti.com>

--- camera/OMXCameraAdapter/OMXCameraAdapter.cpp
+++ camera/OMXCameraAdapter/OMXCameraAdapter.cpp
@@ -2796,6 +2796,7 @@ OMX_ERRORTYPE OMXCameraAdapter::SignalEvent(OMX_IN OMX_HANDLETYPE hComponent,
 {
     Mutex::Autolock lock(mEventLock);
     TIUTILS::Message *msg;
+    bool eventSignalled = false;
 
     LOG_FUNCTION_NAME;
 
@@ -2828,6 +2829,19 @@ OMX_ERRORTYPE OMXCameraAdapter::SignalEvent(OMX_IN OMX_HANDLETYPE hComponent,
         {
         CAMHAL_LOGDA("Event queue empty!!!");
         }
+
+    // Special handling for any unregistered events
+    if (!eventSignalled) {
+        // Handling for focus callback
+        if ((nData2 == OMX_IndexConfigCommonFocusStatus) &&
+            (eEvent == (OMX_EVENTTYPE) OMX_EventIndexSettingChanged)) {
+                TIUTILS::Message msg;
+                msg.command = OMXCallbackHandler::CAMERA_FOCUS_STATUS;
+                msg.arg1 = NULL;
+                msg.arg2 = NULL;
+                mOMXCallbackHandler->put(&msg);
+        }
+    }
 
     LOG_FUNCTION_NAME_EXIT;
 
@@ -3433,6 +3447,11 @@ bool OMXCameraAdapter::OMXCallbackHandler::Handler()
             {
                 ret = mCameraAdapter->OMXCameraAdapterFillBufferDone(( OMX_HANDLETYPE ) msg.arg1,
                                                                      ( OMX_BUFFERHEADERTYPE *) msg.arg2);
+                break;
+            }
+            case OMXCallbackHandler::CAMERA_FOCUS_STATUS:
+            {
+                mCameraAdapter->handleFocusCallback();
                 break;
             }
             case CommandHandler::COMMAND_EXIT:
