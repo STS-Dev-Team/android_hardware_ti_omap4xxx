STS-Dev-Team/android_hardware_ti_omap4xxx

commit cd491d7625950d5140d22af0fcecb7206bac3c3c

CameraHAL: TRUE and FALSE cleanup
Replaced custom boolean TRUE and FALSE string tokens
with CameraParameters::TRUE and FALSE.

Removed tokens:
- VIDEO_SNAPSHOT_SUPPORTED
- VIDEO_SNAPSHOT_UNSUPPORTED
- ZOOM_SUPPORTED
- ZOOM_UNSUPPORTED
- MEASUREMENT_ENABLE
- MEASUREMENT_DISABLE
- SHUTTER_ENABLE
- SHUTTER_DISABLE
- BRACKET_ENABLE
- BRACKET_DISABLE
- DEFAULT_LOCK_SUPPORTED
- DEFAULT_LOCK_UNSUPPORTED

Also moved TRUE and FALSE defines from NV12_resize.h to NV12_resize.c.

Must be merged with: http://review.omapzoom.org/#/c/19573/

Signed-off-by: Daniel Levin <dendy@ti.com>

Change-Id: I410c489f3be06cd3bc43c4cedc5a3ba9f9fdaff8

camera/OMXCameraAdapter/OMXCapabilities.cpp
@@ -978,11 +978,11 @@ status_t OMXCameraAdapter::insertZoomStages(CameraProperties::Properties* params
     params->set(CameraProperties::SUPPORTED_ZOOM_STAGES, zoomStageCount - 1); //As per CTS requirement
 
     if ( 0 == zoomStageCount ) {
-        params->set(CameraProperties::ZOOM_SUPPORTED, TICameraParameters::ZOOM_UNSUPPORTED);
-        params->set(CameraProperties::SMOOTH_ZOOM_SUPPORTED, TICameraParameters::ZOOM_UNSUPPORTED);
+        params->set(CameraProperties::ZOOM_SUPPORTED, CameraParameters::FALSE);
+        params->set(CameraProperties::SMOOTH_ZOOM_SUPPORTED, CameraParameters::FALSE);
     } else {
-        params->set(CameraProperties::ZOOM_SUPPORTED, TICameraParameters::ZOOM_SUPPORTED);
-        params->set(CameraProperties::SMOOTH_ZOOM_SUPPORTED, TICameraParameters::ZOOM_SUPPORTED);
+        params->set(CameraProperties::ZOOM_SUPPORTED, CameraParameters::TRUE);
+        params->set(CameraProperties::SMOOTH_ZOOM_SUPPORTED, CameraParameters::TRUE);
     }
 
     LOG_FUNCTION_NAME_EXIT;
@@ -1497,11 +1497,11 @@ status_t OMXCameraAdapter::insertLocks(CameraProperties::Properties* params, OMX
     LOG_FUNCTION_NAME
 
     if ( caps.bAELockSupported ) {
-        params->set(CameraProperties::AUTO_EXPOSURE_LOCK_SUPPORTED, DEFAULT_LOCK_SUPPORTED);
+        params->set(CameraProperties::AUTO_EXPOSURE_LOCK_SUPPORTED, CameraParameters::TRUE);
     }
 
     if ( caps.bAWBLockSupported ) {
-        params->set(CameraProperties::AUTO_WHITEBALANCE_LOCK_SUPPORTED, DEFAULT_LOCK_SUPPORTED);
+        params->set(CameraProperties::AUTO_WHITEBALANCE_LOCK_SUPPORTED, CameraParameters::FALSE);
     }
 
     LOG_FUNCTION_NAME_EXIT
@@ -1780,11 +1780,11 @@ status_t OMXCameraAdapter::insertVideoSnapshotSupported(CameraProperties::Proper
 
     if (caps.bStillCapDuringVideoSupported)
     {
-        params->set(CameraProperties::VIDEO_SNAPSHOT_SUPPORTED, TICameraParameters::VIDEO_SNAPSHOT_SUPPORTED);
+        params->set(CameraProperties::VIDEO_SNAPSHOT_SUPPORTED, CameraParameters::TRUE);
     }
     else
     {
-        params->set(CameraProperties::VIDEO_SNAPSHOT_SUPPORTED, TICameraParameters::VIDEO_SNAPSHOT_UNSUPPORTED);
+        params->set(CameraProperties::VIDEO_SNAPSHOT_SUPPORTED, CameraParameters::FALSE);
     }
 
     LOG_FUNCTION_NAME_EXIT;

camera/CameraHal.cpp
@@ -739,14 +739,14 @@ int CameraHal::setParameters(const CameraParameters& params)
 
         if( (valstr = params.get(TICameraParameters::KEY_MEASUREMENT_ENABLE)) != NULL )
             {
-            CAMHAL_LOGDB("Measurements set to %s", params.get(TICameraParameters::KEY_MEASUREMENT_ENABLE));
+            CAMHAL_LOGDB("Measurements set to %s", valstr);
             mParameters.set(TICameraParameters::KEY_MEASUREMENT_ENABLE, valstr);
 
-            if (strcmp(valstr, (const char *) TICameraParameters::MEASUREMENT_ENABLE) == 0)
+            if (strcmp(valstr, CameraParameters::TRUE) == 0)
                 {
                 mMeasurementEnabled = true;
                 }
-            else if (strcmp(valstr, (const char *) TICameraParameters::MEASUREMENT_DISABLE) == 0)
+            else if (strcmp(valstr, CameraParameters::FALSE) == 0)
                 {
                 mMeasurementEnabled = false;
                 }
@@ -988,7 +988,7 @@ int CameraHal::setParameters(const CameraParameters& params)
         CAMHAL_LOGDB("Negative bracketing range %d", mBracketRangeNegative);
 
         if( ( (valstr = params.get(TICameraParameters::KEY_TEMP_BRACKETING)) != NULL) &&
-            ( strcmp(valstr, TICameraParameters::BRACKET_ENABLE) == 0 )) {
+            ( strcmp(valstr, CameraParameters::TRUE) == 0 )) {
             if ( !mBracketingEnabled ) {
                 CAMHAL_LOGDA("Enabling bracketing");
                 mBracketingEnabled = true;
@@ -998,7 +998,7 @@ int CameraHal::setParameters(const CameraParameters& params)
             adapterParams.set(TICameraParameters::KEY_TEMP_BRACKETING, valstr);
             mParameters.set(TICameraParameters::KEY_TEMP_BRACKETING, valstr);
         } else if ( ( (valstr = params.get(TICameraParameters::KEY_TEMP_BRACKETING)) != NULL ) &&
-            ( strcmp(valstr, TICameraParameters::BRACKET_DISABLE) == 0 )) {
+            ( strcmp(valstr, CameraParameters::FALSE) == 0 )) {
             CAMHAL_LOGDA("Disabling bracketing");
 
             adapterParams.set(TICameraParameters::KEY_TEMP_BRACKETING, valstr);
@@ -1022,7 +1022,7 @@ int CameraHal::setParameters(const CameraParameters& params)
         }
 
         if( ( (valstr = params.get(TICameraParameters::KEY_SHUTTER_ENABLE)) != NULL ) &&
-            ( strcmp(valstr, TICameraParameters::SHUTTER_ENABLE) == 0 ))
+            ( strcmp(valstr, CameraParameters::TRUE) == 0 ))
             {
             CAMHAL_LOGDA("Enabling shutter sound");
 
@@ -1031,7 +1031,7 @@ int CameraHal::setParameters(const CameraParameters& params)
             mParameters.set(TICameraParameters::KEY_SHUTTER_ENABLE, valstr);
             }
         else if ( ( (valstr = params.get(TICameraParameters::KEY_SHUTTER_ENABLE)) != NULL ) &&
-            ( strcmp(valstr, TICameraParameters::SHUTTER_DISABLE) == 0 ))
+            ( strcmp(valstr, CameraParameters::FALSE) == 0 ))
             {
             CAMHAL_LOGDA("Disabling shutter sound");
 

camera/NV12_resize.c
@@ -12,6 +12,22 @@
 #define STRIDE 4096
 #include <utils/Log.h>
 
+/*----------------------------------------------------------------------------
+    Define : TRUE/FALSE for boolean operations
+----------------------------------------------------------------------------*/
+
+#ifndef TRUE
+    #define TRUE 1
+#endif
+
+#ifndef FALSE
+    #define FALSE 0
+#endif
+
+#ifndef NULL
+   #define NULL 0
+#endif
+
 /*==========================================================================
 * Function Name  : VT_resizeFrame_Video_opt2_lp
 *

camera/OMXCameraAdapter/OMX3A.cpp
@@ -27,11 +27,6 @@
 
 #include <cutils/properties.h>
 
-#undef TRUE
-#undef FALSE
-#define TRUE "true"
-#define FALSE "false"
-
 #define METERING_AREAS_RANGE 0xFF
 
 static const char PARAM_SEP[] = ",";
@@ -298,12 +293,12 @@ status_t OMXCameraAdapter::setParameters3A(const CameraParameters &params,
         }
 
     str = params.get(CameraParameters::KEY_AUTO_EXPOSURE_LOCK_SUPPORTED);
-    if ( (str != NULL) && (!strcmp(str, "true")) )
+    if ( (str != NULL) && (!strcmp(str, CameraParameters::TRUE)) )
       {
         OMX_BOOL lock = OMX_FALSE;
         mUserSetExpLock = OMX_FALSE;
         str = params.get(CameraParameters::KEY_AUTO_EXPOSURE_LOCK);
-        if (str && ((strcmp(str, "true")) == 0))
+        if (str && ((strcmp(str, CameraParameters::TRUE)) == 0))
           {
             CAMHAL_LOGVA("Locking Exposure");
             lock = OMX_TRUE;
@@ -323,12 +318,12 @@ status_t OMXCameraAdapter::setParameters3A(const CameraParameters &params,
       }
 
     str = params.get(CameraParameters::KEY_AUTO_WHITEBALANCE_LOCK_SUPPORTED);
-    if ( (str != NULL) && (!strcmp(str, "true")) )
+    if ( (str != NULL) && (!strcmp(str, CameraParameters::TRUE)) )
       {
         OMX_BOOL lock = OMX_FALSE;
         mUserSetWbLock = OMX_FALSE;
         str = params.get(CameraParameters::KEY_AUTO_WHITEBALANCE_LOCK);
-        if (str && ((strcmp(str, "true")) == 0))
+        if (str && ((strcmp(str, CameraParameters::TRUE)) == 0))
           {
             CAMHAL_LOGVA("Locking WhiteBalance");
             lock = OMX_TRUE;
@@ -347,11 +342,11 @@ status_t OMXCameraAdapter::setParameters3A(const CameraParameters &params,
       }
 
     str = params.get(TICameraParameters::KEY_AUTO_FOCUS_LOCK);
-    if (str && (strcmp(str, TRUE) == 0) && (mParameters3A.FocusLock != OMX_TRUE)) {
+    if (str && (strcmp(str, CameraParameters::TRUE) == 0) && (mParameters3A.FocusLock != OMX_TRUE)) {
         CAMHAL_LOGVA("Locking Focus");
         mParameters3A.FocusLock = OMX_TRUE;
         setFocusLock(mParameters3A);
-    } else if (str && (strcmp(str, FALSE) == 0) && (mParameters3A.FocusLock != OMX_FALSE)) {
+    } else if (str && (strcmp(str, CameraParameters::FALSE) == 0) && (mParameters3A.FocusLock != OMX_FALSE)) {
         CAMHAL_LOGVA("UnLocking Focus");
         mParameters3A.FocusLock = OMX_FALSE;
         setFocusLock(mParameters3A);
@@ -481,7 +476,7 @@ static bool isFlashDisabled() {
 
     char value[PROPERTY_VALUE_MAX];
     if (property_get("camera.flash_off", value, NULL) &&
-        (!strcasecmp(value, "true") || !strcasecmp(value, "1"))) {
+        (!strcasecmp(value, CameraParameters::TRUE) || !strcasecmp(value, "1"))) {
         LOGW("flash is disabled for testing purpose");
         return true;
     }
@@ -1519,7 +1514,7 @@ status_t OMXCameraAdapter::set3ALock(OMX_BOOL toggleExp, OMX_BOOL toggleWb, OMX_
     }
     else
     {
-        const char *lock_state_exp = toggleExp ? TRUE : FALSE;
+        const char *lock_state_exp = toggleExp ? CameraParameters::TRUE : CameraParameters::FALSE;
         CAMHAL_LOGDA("Exposure Lock GetConfig successfull");
 
         /* Apply locks only when not applied already */
@@ -1565,7 +1560,7 @@ status_t OMXCameraAdapter::set3ALock(OMX_BOOL toggleExp, OMX_BOOL toggleWb, OMX_
     }
     else
     {
-        const char *lock_state_wb = toggleWb ? TRUE : FALSE;
+        const char *lock_state_wb = toggleWb ? CameraParameters::TRUE : CameraParameters::FALSE;
         CAMHAL_LOGDA("WhiteBalance Lock GetConfig successfull");
 
         /* Apply locks only when not applied already */

camera/OMXCameraAdapter/OMXCameraAdapter.cpp
@@ -575,11 +575,11 @@ status_t OMXCameraAdapter::setParameters(const CameraParameters &params)
 
     if ( (valstr = params.get(TICameraParameters::KEY_MEASUREMENT_ENABLE)) != NULL )
         {
-        if (strcmp(valstr, (const char *) TICameraParameters::MEASUREMENT_ENABLE) == 0)
+        if (strcmp(valstr, CameraParameters::TRUE) == 0)
             {
             mMeasurementEnabled = true;
             }
-        else if (strcmp(valstr, (const char *) TICameraParameters::MEASUREMENT_DISABLE) == 0)
+        else if (strcmp(valstr, CameraParameters::FALSE) == 0)
             {
             mMeasurementEnabled = false;
             }

camera/OMXCameraAdapter/OMXCapture.cpp
@@ -140,7 +140,7 @@ status_t OMXCameraAdapter::setParametersCapture(const CameraParameters &params,
 
     str = params.get(TICameraParameters::KEY_TEMP_BRACKETING);
     if ( ( str != NULL ) &&
-         ( strcmp(str, TICameraParameters::BRACKET_ENABLE ) == 0 ) ) {
+         ( strcmp(str, CameraParameters::TRUE) == 0 ) ) {
 
         if ( !mBracketingSet ) {
             mPendingCaptureSettings |= SetExpBracket;

camera/OMXCameraAdapter/OMXDefaults.cpp
@@ -63,19 +63,17 @@ const char OMXCameraAdapter::DEFAULT_MAX_FOCUS_AREAS[] = "1";
 const char OMXCameraAdapter::DEFAULT_SATURATION[] = "100";
 const char OMXCameraAdapter::DEFAULT_SCENE_MODE[] = "auto";
 const char OMXCameraAdapter::DEFAULT_SHARPNESS[] = "100";
-const char OMXCameraAdapter::DEFAULT_VSTAB[] = "false";
-const char OMXCameraAdapter::DEFAULT_VNF[] = "false";
+const char * OMXCameraAdapter::DEFAULT_VSTAB = CameraParameters::FALSE;
+const char * OMXCameraAdapter::DEFAULT_VNF = CameraParameters::FALSE;
 const char OMXCameraAdapter::DEFAULT_WB[] = "auto";
 const char OMXCameraAdapter::DEFAULT_ZOOM[] = "0";
 const char OMXCameraAdapter::DEFAULT_MAX_FD_HW_FACES[] = __STRINGIFY(MAX_NUM_FACES_SUPPORTED);
 const char OMXCameraAdapter::DEFAULT_MAX_FD_SW_FACES[] = "0";
 const char OMXCameraAdapter::DEFAULT_HOR_ANGLE[] = "54.8";
 const char OMXCameraAdapter::DEFAULT_VER_ANGLE[] = "42.5";
-const char OMXCameraAdapter::DEFAULT_AE_LOCK[] = "false";
-const char OMXCameraAdapter::DEFAULT_AWB_LOCK[] = "false";
+const char * OMXCameraAdapter::DEFAULT_AE_LOCK = CameraParameters::FALSE;
+const char * OMXCameraAdapter::DEFAULT_AWB_LOCK = CameraParameters::FALSE;
 const char OMXCameraAdapter::DEFAULT_MAX_NUM_METERING_AREAS[] = "0";
-const char OMXCameraAdapter::DEFAULT_LOCK_SUPPORTED[] = "true";
-const char OMXCameraAdapter::DEFAULT_LOCK_UNSUPPORTED[] = "false";
 const char OMXCameraAdapter::DEFAULT_VIDEO_SIZE[] = "1920x1080";
 const char OMXCameraAdapter::DEFAULT_PREFERRED_PREVIEW_SIZE_FOR_VIDEO[] = "1920x1080";
 const char OMXCameraAdapter::DEFAULT_SENSOR_ORIENTATION[] = "0";

camera/TICameraParameters.cpp
@@ -82,14 +82,6 @@ const char TICameraParameters::RAW_WIDTH[] = "raw-width";
 const char TICameraParameters::RAW_HEIGHT[] = "raw-height";
 const char TICameraParameters::KEY_FILENAME_TIMESTAMP[] = "captured-filename";
 
-//TI extensions for enabling/disabling measurement
-const char TICameraParameters::MEASUREMENT_ENABLE[] = "enable";
-const char TICameraParameters::MEASUREMENT_DISABLE[] = "disable";
-
-//TI extensions for zoom
-const char TICameraParameters::ZOOM_SUPPORTED[] = "true";
-const char TICameraParameters::ZOOM_UNSUPPORTED[] = "false";
-
 // TI extensions for Stereo Mode
 const char TICameraParameters::KEY_S3D_PRV_FRAME_LAYOUT[] = "s3d-prv-frame-layout";
 const char TICameraParameters::KEY_S3D_PRV_FRAME_LAYOUT_VALUES[] = "s3d-prv-frame-layout-values";
@@ -120,14 +112,6 @@ const char TICameraParameters::KEY_EXIF_MAKE[] = "exif-make";
 const char TICameraParameters::KEY_GPS_MAPDATUM[] = "gps-mapdatum";
 const char TICameraParameters::KEY_GPS_VERSION[] = "gps-version";
 const char TICameraParameters::KEY_GPS_DATESTAMP[] = "gps-datestamp";
-
-//TI extensions for enabling/disabling shutter sound
-const char TICameraParameters::SHUTTER_ENABLE[] = "true";
-const char TICameraParameters::SHUTTER_DISABLE[] = "false";
-
-//TI extensions for Temporal Bracketing
-const char TICameraParameters::BRACKET_ENABLE[] = "enable";
-const char TICameraParameters::BRACKET_DISABLE[] = "disable";
 
 //TI extensions to Image post-processing
 const char TICameraParameters::IPP_LDCNSF[] = "ldc-nsf";
@@ -227,10 +211,5 @@ const char TICameraParameters::KEY_MECHANICAL_MISALIGNMENT_CORRECTION_VALUES[] =
 
 const char TICameraParameters::MECHANICAL_MISALIGNMENT_CORRECTION_ENABLE[] = "enable";
 const char TICameraParameters::MECHANICAL_MISALIGNMENT_CORRECTION_DISABLE[] = "disable";
-
-//TI extensions for video snapshot
-const char TICameraParameters::VIDEO_SNAPSHOT_SUPPORTED[] = "true";
-const char TICameraParameters::VIDEO_SNAPSHOT_UNSUPPORTED[] = "false";
-
 };
 

commit c58944845dcad9f0968633019542de31e22539e3

CameraHal: Fix AE/AWB lock capability
Fix for regression introduced with:
cd491d7 CameraHAL: TRUE and FALSE cleanup

Currently AE/AWB lock OMX capabilities are not set properly.
Especially, if Ducati returned AWB lock "Supported",
the corresponding cababilities became set to "FALSE",
but should be "TRUE" instead.

Change-Id: I83ca77e25003e395a486d9e62142d56e631a19fa
Signed-off-by: Vladimir Petrov <vppetrov@mm-sol.com>

camera/OMXCameraAdapter/OMXCapabilities.cpp
@@ -1498,9 +1498,13 @@ status_t OMXCameraAdapter::insertLocks(CameraProperties::Properties* params, OMX
 
     if ( caps.bAELockSupported ) {
         params->set(CameraProperties::AUTO_EXPOSURE_LOCK_SUPPORTED, CameraParameters::TRUE);
+    } else {
+        params->set(CameraProperties::AUTO_EXPOSURE_LOCK_SUPPORTED, CameraParameters::FALSE);
     }
 
     if ( caps.bAWBLockSupported ) {
+        params->set(CameraProperties::AUTO_WHITEBALANCE_LOCK_SUPPORTED, CameraParameters::TRUE);
+    } else {
         params->set(CameraProperties::AUTO_WHITEBALANCE_LOCK_SUPPORTED, CameraParameters::FALSE);
     }
 