166ca118545b85bdbce466c8b666ff861bee615c
Jean Johnson <jean-johnson@ti.com>
2012-05-03 12:27:45 -0500

    Modify Android.mk to ensure code only OMAP4/OMAP5
    Modifying the Android.mk files to ensure code is only run
    for OMAP4 and OMAP5 boards.
    
    Change-Id: I453a2b00051e15c4d786fd0086cb120961ae76e5
    Signed-off-by: Jean Johnson <jean-johnson@ti.com>

--- libstagefrighthw/Android.mk
+++ libstagefrighthw/Android.mk
@@ -1,4 +1,8 @@
+# Only applicable for OMAP4 and OMAP5 boards.
+# First eliminate OMAP3 and then ensure that this is not used
+# for customer boards
 ifneq ($(TARGET_BOARD_PLATFORM),omap3)
+ifeq ($(findstring omap, $(TARGET_BOARD_PLATFORM)),omap)
 
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
@@ -20,4 +24,5 @@ LOCAL_MODULE := libstagefrighthw
 
 include $(BUILD_HEAPTRACKED_SHARED_LIBRARY)
 
+endif
 endif
