96953466067743a04f082b73427880eedfcf848e
Jean Johnson <jean-johnson@ti.com>
2012-05-03 15:10:54 -0500

    Modifying Android.mk to enable for OMAP4&5 only
    Modifying the Android.mk files in the security area to ensure that
    they are only run for OMAP4/5.  They are not used by OMAP3 and this
    patch ensures that they will not be compiled for customer boards.
    
    Change-Id: Ie402beec6421f107459a756463812e9b3117a855
    Signed-off-by: Jean Johnson <jean-johnson@ti.com>

--- security/tf_daemon/Android.mk
+++ security/tf_daemon/Android.mk
@@ -1,4 +1,8 @@
+# Only applicable for OMAP4 and OMAP5 boards.
+# First eliminate OMAP3 and then ensure that this is not used
+# for customer boards
 ifneq ($(TARGET_BOARD_PLATFORM),omap3)
+ifeq ($(findstring omap, $(TARGET_BOARD_PLATFORM)),omap)
 
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
@@ -30,4 +34,5 @@ LOCAL_STATIC_LIBRARIES := libtee_client_api_driver
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_EXECUTABLE)
+endif
 endif
